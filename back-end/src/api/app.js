5204f8045bfca23515ff2aedac2da109f95dd8a6
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-10-10 19:43:01 -0300

    feat: products route, rota retornando os produtos e status 200

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const product = require('../routes/productsRoute');
 
 const cors = require('cors');
 
 
 const app = express();
 
-
 app.use(cors());
 
 app.use(express.json());
+
+app.use('/products', product);
 
 module.exports = app;
