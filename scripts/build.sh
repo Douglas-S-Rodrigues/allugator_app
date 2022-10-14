666787aa15b9005df2631de145d083f6e922371d
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-10-14 02:31:20 -0300

    fix: limpando as portas para rodar o app

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,7 @@ export NODE_ENV=development
 function db_migrate () {
   printf "\n> ASYNC: dropando o bancoe criando as migration\n"
   (
+    kill -9 $(lsof -t -i:3000) &> /dev/null & kill -9 $(lsof -t -i:3001) &> /dev/null
     cd ./back-end
     npx sequelize-cli db:drop
     npx sequelize-cli db:create
